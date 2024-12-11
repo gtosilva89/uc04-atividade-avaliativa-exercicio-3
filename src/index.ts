a9f5bb0180b51e663ee38c105c5018c1ffc2647a
Gustavo Silveira Silva <silva.gustavo@hotmail.com.br>
2024-12-11 19:28:21 -0300

    exercicio

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log("Olá mundo");
+// A criptografia é um conjunto de técnicas pensadas para proteger uma informação de modo que apenas o emissor e receptor consigam compreendê-la.
+// Uma forma simples de fazer criptografar um vetor de strings é aplicar uma lógica de troca em cada posição do vetor, de modo que apenas o programador conheça a regra,
+// lembrando que a regra aplicada precisa de alguma forma ser reversível.
+// Dessa forma um vetor auxiliar e faça a cópia dos dados do vetor original, porém de forma criptografada e na sequência imprima os dados criptografados no console e por fim,
+// crie outro vetor auxiliar que faça a restauração do texto original, copiando a partir do vetor criptografado, e também faça a impressão no console do mesmo.
